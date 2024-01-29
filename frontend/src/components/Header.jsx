5e13dc048820088ea5d83d1e1c0b692d68415ea7
Pratyanch dongre <69388801+pratyanch-github@users.noreply.github.com>
2024-01-30 02:44:10 +0530

    added error element

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -9,12 +9,16 @@ function Header() {
 
   return (
     <div className='p-4 flex h-16  justify-between items-center gap-4 bg-blue-500'>
-        <div className='logo font-bold border-2 p-2 border-black rounded-full '>
-           <Link to="/">Home </Link> 
+        <Link to="/">
+        <div className='logo font-bold border-2 p-2 border-black bg-blue-500 rounded-full hover:bg-blue-400'>
+           Home 
         </div>
-        <div className='buttons flex m-5 p-2 bg-blue-700 rounded-lg '>
+        </Link> 
+        <div >
            {
-              userPresent ? <Link to={"profile/"+loggedinUserId}><button> 👨‍🦰 My Profile </button></Link>  :   <Link to="login"><button>Login</button></Link>
+              userPresent ?
+               <Link className='buttons flex m-5 p-2 bg-green-400 rounded-lg ' to={"profile/"+loggedinUserId}><button> 👨‍🦰 My Profile </button></Link>  : 
+               <Link className='buttons flex m-5 p-2 bg-blue-700 rounded-lg ' to="login"><button>Login</button></Link>
            } 
         </div>
     </div>
